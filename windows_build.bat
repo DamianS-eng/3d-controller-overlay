ee1fe75f1dfedecd22c5732b17bfbdab5412e491
Damian <damiansuaz@gmail.com>
2025-11-19 11:49:38 -0500

    Fix windres

--- windows_build.bat
+++ windows_build.bat
@@ -5,20 +5,12 @@ REM Ensure MinGW is in PATH (adjust if installed elsewhere)
 set PATH=C:\tools\mingw64\bin;C:\tools\mingw32\bin;C:\ProgramData\chocolatey\bin;%PATH%
 
 REM Compile resource file if present
-if exist 3dco.rc (
-    windres -i 3dco.rc -O coff -o 3dco.res
-)
+if exist 3dco.res del 3dco.res
+
+windres 3dco.rc -o 3dco.res
 
 REM Run g++ with explicit file expansion
-g++ -std=c++17 -g -mwindows -Wall ^
-  src\glad.c ^
-  src\imgui\*.cpp ^
-  src\*.cpp ^
-  -I include -I src\imgui ^
-  -o 3dco.exe ^
-  3dco.res ^
-  -L lib ^
-  -lSDL2 -lglfw3 -lmingw32 -lgdi32 -lopengl32
+g++ -std=c++17 -g -mwindows -Wall src\glad.c src\imgui\*.cpp src\*.cpp -I include -I src\imgui -o 3dco.exe 3dco.res -L lib -lSDL2 -lglfw3 -lmingw32 -lgdi32 -lopengl32
 
 REM Check for errors
 if %ERRORLEVEL% neq 0 (
